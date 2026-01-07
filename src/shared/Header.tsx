04d493f18c8653cbcccbcfca9f6aba4aa66e62a1
tonmoy3673 <meheditonmoy917@gmail.com>
2026-01-07 22:33:57 +0600

    :art: design book sell button and added dropdown

--- src/shared/Header.tsx
+++ src/shared/Header.tsx
@@ -1,10 +1,60 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
-import { Search } from "lucide-react";
+import { Lock, Search, User } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 const Header = () => {
+  const handleLoginClick =()=>{
+    
+  }
+  const user : any = {
+    profilePicture: "",
+    name:'',
+    email:''
+  };
+
+  const userPlaceholder = "";
+  // ========== menuItems ===========//
+  const menuItems = [
+    ...(user && user ? [
+      {
+        href:"account/profile",
+        content:(
+          <div className="flex space-x-4 items-center p-2 border-b">
+               <Avatar className="w-12 h-12 -ml-2 rounded-full">
+                {user?.profilePicture ? (
+                  <AvatarImage alt="user_image"></AvatarImage>
+                ) : (
+                  <AvatarFallback>Tonmoy</AvatarFallback>
+                ) }
+              </Avatar>
+              {/* =========== name and email ============ */}
+              <div className="flex flex-col">
+                <span className="font-semibold text-base"> 
+                  {user.name}
+                </span>
+                 <span className="font-semibold text-xs text-gray-500"> 
+                  {user.email}
+                </span>
+              </div>
+          </div>
+        )
+      }
+    ]:[
+      {
+        icon:<Lock className="h-5 w-5"/>,
+        label:"Login/Signup",
+        onClick:handleLoginClick
+      }
+    ])
+  ];
   return (
     <header className="border-b bg-white sticky top-0 z-50">
       {/* ============ desktop view ========== */}
@@ -23,7 +73,6 @@ const Header = () => {
           <div className="relative w-full ">
             <Input
               type="text"
-              
               className="w-full pr-10 "
               placeholder="Book Name / Author / Subject / Publisher"
             />
@@ -36,12 +85,34 @@ const Header = () => {
             </Button>
           </div>
         </div>
-        {/* ============= book sell ============ */}
+        {/* ============= book sell button ============ */}
         <div className="flex items-center gap-4">
-        <Link href='/book-sell'>
-        </Link>
+          <Link href="/book-sell">
+            <Button className="bg-[#00897b] cursor-pointer text-white hover:bg-[#00695C] transition-colors duration-200">
+              Sell Used Book
+            </Button>
+          </Link>
         </div>
+        {/* ================= dropdown ============= */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost">
+              <Avatar className="w-8 h-8 rounded-full">
+                {user?.profilePicture ? (
+                  <AvatarImage alt="user_image"></AvatarImage>
+                ) : userPlaceholder ? (
+                  <AvatarFallback>Tonmoy</AvatarFallback>
+                ) : (
+                  <User className="ml-2 mt-2" />
+                )}
+              </Avatar>
+              My Account
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-80 p-2">
 
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </header>
   );
